e9e459561063b3b3d572284e20c7964f56b9ce9b
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-18 16:13:20 +0800

    Create 6320502436_6_2.cpp

---
+++ 6320502436_6_2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a,b=0,c=0,d,e;
+	scanf("%d",&a)
+	char A[2][a]={};
+	scanf("%s",A[0]);
+	scanf("%s",A[1]);
+	for(d=0;d<a;d++)
+	{
+		if(A[0][d]==A[1][d])
+		{
+			b++
+		}
+		else {
+			c++
+		}
+	}
+	printf("%d %d",b,c);
+	
+	
+}
