d1b68c7026865bd84d6969262be4405d18423089
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-18 17:38:38 +0800

    Create Untitled7.cpp

---
+++ Untitled7.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f,max=0,x,y,max2=0;
+	scanf("%d",&a);
+	int A[a]={};
+	for(b=0;b<a;b++)
+	{
+		scanf("%d %d",&c,&d);
+		A[c-1]=A[c-1]+d;
+	}
+	for(b=0;b<a;b++)
+	{
+		if(A[b]>=max)
+		{
+			x=b;
+			max=A[b];
+		}
+		if(A[b]>max2&&b!=x)
+		{
+			y=b;
+			max2=A[b];
+		}
+}
+printf("%d %d",x+1,y+1);
+	
+}
